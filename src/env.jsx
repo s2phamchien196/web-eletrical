7a0d28bef720b3eeef003a627d4be44381309901
Pham Chien <196phamminhchien@gmail.com>
2024-05-01 10:50:10 +0700

    update code

--- src/env.jsx
+++ src/env.jsx
@@ -2,7 +2,7 @@ const env = process.env;
 
 export const getApiURL = () => {
   if (env.NODE_ENV === 'production') {
-    return 'https://api.vatusangquyen.vn'
+    return 'https://api.vattusangquyen.vn'
   } else {
     return 'http://localhost:4000'
   }
